a6a398c83c32d9948fd073f7133ff9ffbb0dd3da
Bogdan Krintal <87840160+theneedforned@users.noreply.github.com>
2025-07-10 10:17:03 -0400

    Update tt.py
    Cleanups in code

--- tt.py
+++ tt.py
@@ -7,10 +7,6 @@ import time
 import argparse
 import math
 
-'''
-parser = argparse.ArgumentParser(description='Do a typing test!')
-parser.add_argument('--mode', help='set mode of the typing test')
-'''
 toprintout = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboreetdolore magna aliqua. Lorem sed risus ultricies tristique. Ut sem nulla pharetra diam sit amet nisl suscipit.'
 x = 0
 
@@ -43,42 +39,20 @@ def main(stdscr):
         elif result != 127 and result == ord(toprintout[x]):
             x += 1
         
-        '''    
-        if x%145==0:
-            r += 1
-            typing.refresh(r, 0, 3, 5, 5, 150)
-        '''
         typing.clear()
-        #typingwin.addstr(0, 0, curses.keyname(result))
         typing.addstr(0, 0, toprintout[:x], reg)
         print("{}, {}".format(x//100, x%100))
         typing.addstr(x//100, x%100, toprintout[x:])
-        '''
-        if x//100 == 1:
-            typing.addstr(x//100, x-(100 * x//100), toprintout[x:])
-        else:
-            typing.addstr(0, x, toprintout[x:])
-        '''
+
         stdscr.addstr(0, 20, 'col: {}, row: {}'.format(x//100, x%100))
         stdscr.addstr(0, 40, "current WPM: " + str((x / 5) / ((time.time() - timenow)/60)))
         stdscr.refresh()
         typing.refresh(x//100, 0, 3, w1, 5, w2)
     
 
-#print(curses.get_tabsize())
 timenow = time.time()
 try:
     wrapper(main)
 except:
     print("Could not run program, try resizing your terminal to at least 105x17")
 print('Finished! Your gross WPM is {}'.format((x / 5 ) / ((time.time() - timenow)/60)))
-
-
-'''
-import pathlib
-
-p = pathlib.Path(__file__)
-
-print(p.parent)
-GODSEND
-'''
